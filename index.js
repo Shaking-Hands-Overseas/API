97e7214a320b4bf146d9c839e7e36d1123ac3cf2
Joel Garcia <joel.garcia1202@hotmail.com>
2021-09-09 22:10:29 +0200

    Update

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const PORT = 8080;
 require("./routes")(app);
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(
     PORT,
     () => console.log(`it's alive in on http://localhost:${PORT}`)
 )
+
 app.get('/', (req, res) => {
     res.status(200).send({
         "welcome": 'Shaking Hands Overseas'
