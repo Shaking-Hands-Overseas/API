5f7571394ddc4a77680c568d4cadf4f3d89105bf
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-04-02 12:00:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin node
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+//const PORT = process.env.PORT;
+const PORT = 8080;
 //const mysql = require('mysql'); // Not in use at the moment
 
 // IMPORT ALL THE ROUTES
 require("./routes")(app);
 
+const LOGGER = function(req, res, next){
+    next()
+}
+
+app.use(LOGGER)
 app.use(express.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
