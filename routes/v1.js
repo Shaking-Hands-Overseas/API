8d4a49d1ac302830a7734b9262330453b1eeb4c9
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-04-11 20:24:39 +0200

    Added buttons

--- routes/v1.js
+++ routes/v1.js
@@ -6,6 +6,7 @@ const app = express();
 //Json files
 var status = require('../Variables/status.json')
 var Finger = require('../Variables/Finger.json')
+var buttons = require('../Variables/Buttons.json')
 
 router.post('/servo', (req, res) => {
     Info = req.body;
@@ -14,8 +15,6 @@ router.post('/servo', (req, res) => {
     status["s3"] = Info["s3"];
     status["s4"] = Info["s4"];
     status["s5"] = Info["s5"];
-    status["s6"] = Info["s6"];
-    status["s7"] = Info["s7"];
     res.status(200).send(status)
 });
 
@@ -26,9 +25,13 @@ router.post('/custom', (req, res) => {
     Finger["2"] = custom["F3"];
     Finger["3"] = custom["F4"];
     Finger["4"] = custom["F5"];
-    Finger["5"] = custom["F6"];
-    Finger["6"] = custom["F7"];
     res.status(200).send( Finger )
+})
+router.post('/buttons', (req, res) => {
+    but = req.body;
+    buttons["b1"] = but["b1"];
+    buttons["b2"] = but["b2"];
+    res.status(200).send( buttons )
 })
 
 router.get('/receiver', (req, res) => {
@@ -38,8 +41,6 @@ router.get('/receiver', (req, res) => {
         "s3": status[Finger["2"]], 
         "s4": status[Finger["3"]], 
         "s5": status[Finger["4"]], 
-        "s6": status[Finger["5"]],
-        "s7": status[Finger["6"]]
     })
 });
 
